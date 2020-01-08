e56a3894374c70b3a15b126bf6940f464f55f0da
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-01-08 18:17:15 +0900

    Added click group

--- src/ai/backend/cli/__main__.py
+++ src/ai/backend/cli/__main__.py
@@ -1,2 +1,18 @@
+import click
+
+
+@click.group()
 def main():
-    print("Hello World !")
+    pass
+
+
+@main.command()
+def mgr():
+    '''Manager CLI'''
+    click.echo(f"Hello Manager")
+
+
+@main.command()
+def ag():
+    '''Agent CLI'''
+    click.echo(f"Hello Agent")
