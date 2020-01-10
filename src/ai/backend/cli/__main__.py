0d5355c6bc30b140986b1f99e9d5a886570cb8c9
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-01-10 17:01:37 +0900

    Removed unnecessary parts

--- src/ai/backend/cli/__main__.py
+++ src/ai/backend/cli/__main__.py
@@ -8,12 +8,10 @@ def main():
     pass
 
 
-commands = {}
 entry_prefix = 'backendai_cli_v10'
 for entrypoint in pkg_resources.iter_entry_points(entry_prefix):
     # plugin = entrypoint.load()
     # print(dir(entrypoint))
-    commands[entrypoint.name] = entrypoint.load()
+    # TODO: Each package requires different versions of some packages, we have to somehow unify them :)
+    main.add_command(entrypoint.load(), name=entrypoint.name)
     break
-for command in commands:
-    main.add_command(commands[command], name=command)
