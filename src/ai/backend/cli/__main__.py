333048d37b12a1b3e906684c6feab4244b991b5d
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-01-10 16:41:20 +0900

    Added entrypoint and commands by using this entrypoint

--- src/ai/backend/cli/__main__.py
+++ src/ai/backend/cli/__main__.py
@@ -1,18 +1,19 @@
 import click
+import pkg_resources
 
 
 @click.group()
 def main():
+    '''Unified Command Line Interface for Backend.ai'''
     pass
 
 
-@main.command()
-def mgr():
-    '''Manager CLI'''
-    click.echo(f"Hello Manager")
-
-
-@main.command()
-def ag():
-    '''Agent CLI'''
-    click.echo(f"Hello Agent")
+commands = {}
+entry_prefix = 'backendai_cli_v10'
+for entrypoint in pkg_resources.iter_entry_points(entry_prefix):
+    # plugin = entrypoint.load()
+    # print(dir(entrypoint))
+    commands[entrypoint.name] = entrypoint.load()
+    break
+for command in commands:
+    main.add_command(commands[command], name=command)
