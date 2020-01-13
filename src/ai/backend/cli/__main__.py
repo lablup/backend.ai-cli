f5db9ce8ca6fd9528d9e93fa3da365a2511eb1dd
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-01-13 17:47:33 +0900

    Modified code for client.py

--- src/ai/backend/cli/__main__.py
+++ src/ai/backend/cli/__main__.py
@@ -10,8 +10,9 @@ def main():
 
 entry_prefix = 'backendai_cli_v10'
 for entrypoint in pkg_resources.iter_entry_points(entry_prefix):
-    # plugin = entrypoint.load()
-    # print(dir(entrypoint))
-    # TODO: Each package requires different versions of some packages, we have to somehow unify them :)
-    main.add_command(entrypoint.load(), name=entrypoint.name)
-    break
+    if entrypoint.name == "_":
+        plugin = entrypoint.load().commands
+        for command in plugin:
+            main.add_command(plugin[command], name=command)
+    else:
+        main.add_command(entrypoint.load(), name=entrypoint.name)
