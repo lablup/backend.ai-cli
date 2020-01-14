c77f289a312a671164ec43d8247f0290cf99b9b6
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-01-14 11:59:26 +0900

    Added comments

--- src/ai/backend/cli/__main__.py
+++ src/ai/backend/cli/__main__.py
@@ -17,6 +17,7 @@ for entrypoint in pkg_resources.iter_entry_points(entry_prefix):
     elif entrypoint.name == "mgr" or entrypoint.name == "ag":
         main.add_command(entrypoint.load(), name=entrypoint.name)
     else:
+        # for start-server & start-watcher
         plugin = main.commands
         for command in plugin:
             plugin[command].add_command(entrypoint.load(), name=entrypoint.name)
